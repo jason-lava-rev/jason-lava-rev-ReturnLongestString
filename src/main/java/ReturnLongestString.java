c556d2a41d6015ca27bbb8997dcbee92febd93e0
jason-lava-rev <lavandowskij@gmail.com>
2023-03-29 18:16:42 +0000

    add for loop to compare string lengths

--- src/main/java/ReturnLongestString.java
+++ src/main/java/ReturnLongestString.java
@@ -8,6 +8,13 @@ public class ReturnLongestString {
      * @return the longest String within arr. You can assume that there are no ties for the longest string.
      */
     public String longest(String[] arr){
-        return null;
+        String longest = "";
+
+        for (int i = 0; i < arr.length; i++) {
+            if (longest.length() < arr[i].length()) {
+                longest = arr[i];
+            }
+        }
+        return longest;
     }
 }
